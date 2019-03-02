89d876ef774216db798c08480182d75cd5f32da7
Keith Spurr <keithspurr@ksqdesign.com>
2019-03-02 16:21:56 +0000

    first commit

---
+++ Members.js
@@ -0,0 +1,27 @@
+const members = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        status: 'active' 
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jane@gmail.com',
+        status: 'inactive'     
+    },
+    {
+        id: 3,
+        name: 'Bruce Banner',
+        email: 'thehulk@yahoo.com',
+        status: 'inactive'     
+    },
+    {
+        id: 4,
+        name: 'Mary Mapes',
+        email: 'mary@hotmail.com',
+        status: 'active'     
+    }
+];
+module.exports = members;
